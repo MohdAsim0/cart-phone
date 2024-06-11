cb549db93346dbc3f5c1caa4d5c7504071efd4e5
MohdAsim0 <asim75077@gmail.com>
2024-06-11 22:54:58 +0530

    msg

---
+++ src/data.js
@@ -0,0 +1,32 @@
+
+
+export const data = [
+    {
+        image:"https://www.course-api.com/images/cart/phone-1.png",
+        phoneName:"Samsung Galaxy S8",
+        price :"399.99",
+        id:1,
+        cartCount:1
+    },
+    {
+        image:"https://www.course-api.com/images/cart/phone-2.png",
+        phoneName:"Google Pixel",
+        price :"499.99",
+        id:2,
+        cartCount:1
+    },
+    {
+        image:"https://www.course-api.com/images/cart/phone-3.png",
+        phoneName:"Xiaomi Redmi Note 2",
+        price :"699.99",
+        id:3,
+        cartCount:1
+    },
+    {
+        image:"https://www.course-api.com/images/cart/phone-4.png",
+        phoneName:"Samsung Galaxy S7",
+        price :"599.99",
+        id:4,
+        cartCount:1
+    }
+]
